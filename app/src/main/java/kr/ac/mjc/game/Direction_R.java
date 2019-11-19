57e3957c8ee172a9186cf086bec04e6604494490
user <beenjoyhwang@naver.com>
2019-11-19 15:02:51 +0900

    수정1

---
+++ app/src/main/java/kr/ac/mjc/game/Direction_R.java
@@ -0,0 +1,34 @@
+package kr.ac.mjc.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Direction_R extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direction__r);
+
+        Button left = (Button)findViewById(R.id.left);
+        Button right = (Button)findViewById(R.id.right);
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Direction_L_walk.class);
+                startActivity(intent);
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Direction_R.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
