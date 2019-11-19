da23669e9827e78df1b7869d85767330d1c83fe2
user <beenjoyhwang@naver.com>
2019-11-19 15:54:37 +0900

    수정2

--- app/src/main/java/kr/ac/mjc/game/Direction_R_Straight.java
+++ app/src/main/java/kr/ac/mjc/game/Direction_R_Straight.java
@@ -1,4 +1,3 @@
-
 package kr.ac.mjc.game;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,31 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Direction_R extends AppCompatActivity {
+public class Direction_R_Straight extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dirction__r);
+        setContentView(R.layout.activity_direction__r__straight);
 
         Button left = (Button)findViewById(R.id.left);
         Button right = (Button)findViewById(R.id.right);
-        Button straight = (Button)findViewById(R.id.straight);
         left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Direction_L_walk.class);
+                Intent intent = new Intent(getApplicationContext(), Direction_End01.class);
                 startActivity(intent);
-
-            }
-        });
-
-        straight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Direction_R_Straight.class);
-                startActivity(intent);
-
+                Toast.makeText(getApplicationContext(), "심부름 실패",Toast.LENGTH_SHORT).show();
             }
         });
 
